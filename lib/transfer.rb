c2030a1009e7db813f2c307c5227923a6b44cd5a
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-20 21:39:48 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,10 @@
 class Transfer
-  attr_accessor :sender, :receiver, :transfer_amount, :status
+  attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amt)
     @sender = sender 
     @receiver = receiver
     @status = 'pending'
-    @transfer_amount = amt 
+    @amount = amt 
   end
 end
